35cace989893f9b010e9fd5d02cf136e8bbac8da
jefersonsilva01 <jeferson.s.silva1@gmail.com>
2024-03-09 21:05:15 -0300

    Done

--- app.js
+++ app.js
@@ -14,12 +14,61 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
 
-// ...
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      let data = beersFromApi.map((el) => {
+        return {
+          id: el.id,
+          image_url: el.image_url,
+          name: el.name,
+          description: el.description,
+          tagline: el.tagline
+        };
+      })
+      res.render('beers', { data })
+    })
+    .catch(error => console.log(error));
+});
+
+app.get('/beers/:id?', (req, res) => {
+  let id = req.params.id;
+  punkAPI
+    .getBeer(id)
+    .then(data => {
+      console.log(data);
+      res.render('beers', { data })
+    })
+    .catch(error => console.log(error));
+})
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(responseFromAPI => {
+      let data = responseFromAPI.map((el) => {
+        return {
+          image_url: el.image_url,
+          name: el.name,
+          description: el.description,
+          tagline: el.tagline,
+          food: el.food_pairing,
+          brewers: el.brewers_tips
+        };
+      });
+      res.render('random-beer', { data })
+    }
+    )
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
